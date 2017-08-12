cb202c4a60e4150aa09149a4fb14b1d7b83cff2c
xiaomo <xiaomo@xiaomo.info>
2017-08-12 17:14:06 +0800

    删除get/set :princess:

--- Protocol/src/main/java/info/xiaomo/gameCore/protocol/handler/MessageHandler.java
+++ Protocol/src/main/java/info/xiaomo/gameCore/protocol/handler/MessageHandler.java
@@ -3,6 +3,8 @@ package info.xiaomo.gameCore.protocol.handler;
 import com.google.protobuf.InvalidProtocolBufferException;
 import info.xiaomo.gameCore.base.concurrent.AbstractCommand;
 import info.xiaomo.gameCore.protocol.Connection;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * 把今天最好的表现当作明天最新的起点．．～
@@ -18,28 +20,13 @@ import info.xiaomo.gameCore.protocol.Connection;
  * desc  :
  * Copyright(©) 2017 by xiaomo.
  */
+@EqualsAndHashCode(callSuper = true)
+@Data
 public abstract class MessageHandler<M> extends AbstractCommand {
     protected Connection connection;
     protected M message;
 
-    public Connection getConnection() {
-        return this.connection;
-    }
-
-    public void setConnection(Connection connection) {
-        this.connection = connection;
-    }
-
-    public M getMessage() {
-        return this.message;
-    }
-
-    public void setMessage(M message) {
-        this.message = message;
-    }
-
-    public abstract M decode(byte[] paramArrayOfByte)
-            throws InvalidProtocolBufferException;
+    public abstract M decode(byte[] paramArrayOfByte) throws InvalidProtocolBufferException;
 
     protected abstract void handMessage(M paramM);
 
