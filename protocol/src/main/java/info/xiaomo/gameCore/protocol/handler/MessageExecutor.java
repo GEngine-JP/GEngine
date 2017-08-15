ab636ddc6b2d60a9612bdf07943d4f2fcb8bd03b
xiaomo <xiaomo@xiaomo.info>
2017-08-15 18:02:16 +0800

    server

--- protocol/src/main/java/info/xiaomo/gameCore/protocol/handler/MessageExecutor.java
+++ protocol/src/main/java/info/xiaomo/gameCore/protocol/handler/MessageExecutor.java
@@ -1,15 +1,15 @@
 package info.xiaomo.gameCore.protocol.handler;
 
 
-import info.xiaomo.gameCore.protocol.entity.BaseMsg;
+import info.xiaomo.gameCore.protocol.AbstractHandler;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 
 
 public interface MessageExecutor {
-    void doCommand(Channel channel, BaseMsg msg) throws Exception;
+    void doCommand(Channel channel, AbstractHandler handler) throws Exception;
 
-    void connected(ChannelHandlerContext ctx );
+    void connected(ChannelHandlerContext ctx);
 
     void disconnected(ChannelHandlerContext ctx);
 
