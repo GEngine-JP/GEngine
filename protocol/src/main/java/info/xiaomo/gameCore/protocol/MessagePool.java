85d4a929242e19111ea207d0af53150b88c28d5f
xiaomo <xiaomo@xiaomo.info>
2017-08-18 14:37:18 +0800

    change

--- protocol/src/main/java/info/xiaomo/gameCore/protocol/MessagePool.java
+++ protocol/src/main/java/info/xiaomo/gameCore/protocol/MessagePool.java
@@ -1,10 +1,12 @@
 package info.xiaomo.gameCore.protocol;
 
 
+import info.xiaomo.gameCore.base.AbstractHandler;
 import info.xiaomo.gameCore.protocol.message.AbstractMessage;
 
 public interface MessagePool {
 
-	AbstractMessage getMessage(int messageId);
+    AbstractMessage getMessage(int messageId);
 
+    void register(int messageId, Class<? extends AbstractMessage> messageClazz, Class<? extends AbstractHandler> handlerClazz);
 }
