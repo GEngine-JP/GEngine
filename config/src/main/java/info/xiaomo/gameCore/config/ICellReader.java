26562b599635bd657ca1fea03780009daab541cf
DESKTOP-2O6USID\YangQiang <281455776@QQ.COM>
2017-08-24 12:38:23 +0800

    缺少的东西

---
+++ config/src/main/java/info/xiaomo/gameCore/config/ICellReader.java
@@ -0,0 +1,24 @@
+package info.xiaomo.gameCore.config; /**
+ * 创建日期:  2017年08月11日 19:06
+ * 创建作者:  杨 强  <281455776@qq.com>
+ */
+
+/**
+ * 单元格读取器
+ *
+ * @author YangQiang
+ */
+public interface ICellReader<T, R> extends IReader<T, R> {
+    R getDefaultValue(T t);
+
+    IConverter<T, R> getConverter();
+
+    @Override
+    default R read(T t) {
+        IConverter<T, R> converter = getConverter();
+        if (converter != null) {
+            return converter.convert(t);
+        }
+        return getDefaultValue(t);
+    }
+}
